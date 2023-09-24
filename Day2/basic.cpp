0c04855a7303fcef0a0911ffe5575c5a768af89a
Faizan <mohammadfaizan6867@gmail.com>
2023-09-24 11:49:44 -0700

    second commit

---
+++ Day2/basic.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+// int main()
+// {
+//     cout<<"Hello World";
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a;
+//     cout<<"Enter the value of a";
+//     cin>>a;
+//     cout<<"Value entered by user is = "<<a;
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a , b;
+//     cout<<"Enter the vaue of a: \n";
+//     cin>>a;
+//     cout<<"Enter the value of b: \n";
+//     cin>>b;
+//     int sum;
+//     sum = a+b;
+//     cout<<"Sum of two numbers is "<<sum;
+//     return 0;
+
+// }
+
+// int main()
+// {
+//     int a, b, c, d;
+//     cout<<"Enter the divisor\n";
+//     cin>>a;
+//     cout<<"Enter the value of dividend\n";
+//     cin>>b;
+//     c = a/b;
+//     d = a%b;
+//     cout<<"Quotient = "<<c<<"\n";
+//     cout<<"Remainder = "<<d;
+//     return 0;
+// }
+
+// int main()
+// {
+//     cout<<"size of char: "<<sizeof(char)<<"\n";
+//     cout<<"size of int: "<<sizeof(int)<<"\n";
+//     cout<<"size of double: "<<sizeof(double)<<"\n";
+//     cout<<"size of float: "<<sizeof(float)<<"\n";
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a, b;
+//     cout<<"Enter the first number: ";
+//     cin>>a;
+//     cout<<"Enter second number: ";
+//     cin>>b;
+//     int temp;
+//     temp = a;
+//     a = b;
+//     b = temp;
+//     cout<<"After swapping: \n";
+//     cout<<"First num: "<<a<<"\n";
+//     cout<<"Second num: "<<b<<"\n";
+//     return 0;
+// }
+
+// int main()
+// {
+//     char A;
+//     cout<<"Enter a character: ";
+//     cin>>A;
+//     cout<<"ASCII value of: "<<A<<"is "<<int(A);
+//     return 0;
+// }
+
+// int main()
+// {
+//     float a, b, c;
+//     cout<<"Enter the value of first number: ";
+//     cin>>a;
+//     cout<<"Enter the value of second number: ";
+//     cin>>b;
+//     c = a*b;
+//     cout<<"Product of both numbers is: "<<c;
+//     return 0;
+// }

a6dc7e1e57705fce487092a170bd5dd9945ed093
Faizan <mohammadfaizan6867@gmail.com>
2023-10-02 11:47:49 -0700

    Another commit

---
+++ Day7/recursion.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+// int sum(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 0;
+//     }
+//     else{
+//         return n + sum(n-1);
+//     }
+    
+    
+// }
+
+// int main()
+// {
+//     int a;
+//     cout<<"Enter the number: ";
+//     cin>>a;
+//     int res = sum(a);
+//     cout<<"Sum of numbers upto "<<a<<" = "<<res;
+//     return 0;
+// }
+
+// int fact(int n)
+// {
+//     if (n == 0 || n == 1)
+//     {
+//         return 1;
+//     }
+//     else{
+//         return n *fact(n - 1);
+//     }
+    
+// }
+
+// int main()
+// {
+//     int N;
+//     cout<<"Enter the number whose factorial is to be found: ";
+//     cin>>N;
+//     int res = fact(N);
+//     cout<<"Factorial of "<<N<<" = "<<res;
+//     return 0;
+// }
+
+// int gcd(int n, int m)
+// {
+//     int p;
+//     if( n%m ==0 )
+//     {
+//         return m;
+//     }
+//     else{
+//         p = n%m;
+//         n = m;
+//         m = p;
+//         return gcd(n , m);
+        
+//     }
+// }
+
+// int main()
+// {
+//     int a, b;
+//     cout << "Enter first number: ";
+//     cin>>a;
+//     cout<<"Enter the second integer: ";
+//     cin>>b;
+//     int res = gcd(a , b);
+//     cout<<"GCD of given two numbers is: "<<res;
+//     return 0;
+// }
+
+// int power(int n, int m)
+// {
+//     if (m != 0)
+//     {
+//         return ( n * power(n,m-1));
+//     }
+//     else{
+//         return 1;
+//     }
+    
+    
+// }
+
+// int main()
+// {
+//     int a, b;
+//     cout<<"Enter the number: ";
+//     cin>>a;
+//     cout<<"Enter the exponent: ";
+//     cin>> b;
+//     int res = power(a,b);
+//     cout<<a<<" to the power "<<b<<" is "<<res;
+//     return 0;
+
+// }
